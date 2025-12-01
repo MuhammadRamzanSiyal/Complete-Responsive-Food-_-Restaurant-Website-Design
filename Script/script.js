a612f200136ca35c36637a4bc2a1cc8e0ee99e6f
Muhammad Ramzan Siyal <unsalifriendly@gmail.com>
2025-12-01 15:58:32 +0500

    Upload file on github

---
+++ Script/script.js
@@ -0,0 +1,58 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick =() =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll =() =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active')
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active')
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    // grabCursor:true,
+    // loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true,
+    }
+    
+    
+  });
+
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
